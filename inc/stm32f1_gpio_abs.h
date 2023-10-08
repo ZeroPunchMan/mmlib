3ae222c2b7d65fff835ea568c2560b743cfbefc4
lyj <knightlyj@163.com>
2023-10-08 10:53:56 +0800

    stm32 gpio增加pull设置

--- inc/stm32f1_gpio_abs.h
+++ inc/stm32f1_gpio_abs.h
@@ -8,7 +8,7 @@ typedef uint32_t AbsGpioPin_t;
 typedef uint32_t AbsGpioMode_t;
 typedef uint32_t AsbGpioPull_t;
 
-static inline void Mmhl_GpioInit(AbsGpioPort_t port, AbsGpioPin_t pin, AbsGpioMode_t mode)
+static inline void Mmhl_GpioInit(AbsGpioPort_t port, AbsGpioPin_t pin, AbsGpioMode_t mode, AsbGpioPull_t pull)
 {
     LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
 
@@ -16,7 +16,7 @@ static inline void Mmhl_GpioInit(AbsGpioPort_t port, AbsGpioPin_t pin, AbsGpioMo
     GPIO_InitStruct.Mode = mode;
     GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
     GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-    GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+    GPIO_InitStruct.Pull = pull;
     LL_GPIO_Init(port, &GPIO_InitStruct);
 }
 
